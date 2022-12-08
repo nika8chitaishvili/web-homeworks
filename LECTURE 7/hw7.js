231a198b5e2c1ae28b32aac3b2221aa088c1ab08
Nika Chitaishvili <nika.chitaishvili@gmail.com>
2022-12-08 16:30:46 +0400

    update TASK 4

--- LECTURE 7/hw7.js
+++ LECTURE 7/hw7.js
@@ -1,37 +1,48 @@
 /// TASK 1
 
-document.getElementById("hider").addEventListener("click", hideAction);
-
+const hider = addEventListener("click", hideAction);
 let hidden = false;
 
 function hideAction() {
   hidden = !hidden;
   if (hidden) {
-    document.getElementById('text').style.visibility = 'hidden';
+    text.style.visibility = 'hidden';
   } else {
-    document.getElementById('text').style.visibility = 'visible';
+    text.style.visibility = 'visible';
   }
 }
 
 
 /// TASK 2
 
-const card = document.createElement('div');
-card.setAttribute("id","card");
-document.getElementById("task2").appendChild(card);
-
+const card2_3 = document.createElement('div');
+card2_3.setAttribute("id","card2_3");
+task2_3.appendChild(card2_3);
 
 const h2 = document.createElement("h2");
 h2.innerHTML = "Vaniko";
-document.getElementById("card").appendChild(h2);
+card2_3.appendChild(h2);
 
 const aLink = document.createElement("a");
 aLink.setAttribute('href','#');
 aLink.innerHTML = 'Go to Profile';
-document.getElementById("card").appendChild(aLink);
-
+card2_3.appendChild(aLink);
 
 
 /// TASK 3
 
-document.getElementById("card").style.backgroundColor = "red";
+card2_3.style.backgroundColor = "red";
+
+
+
+/// TASK 4 - other method for TASK 2
+
+task4.insertAdjacentHTML(
+  "beforeend",
+  `<div id="card4">
+  <h2>Nika</h2>
+  <a href="#">Go To Profile</a>
+  </div>`
+);
+
+card4.style.backgroundColor = "green";
