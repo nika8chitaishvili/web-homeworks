0034962fd1e0acb5d12ea0768a90a154e348c3f9
Nika Chitaishvili <nika.chitaishvili@gmail.com>
2022-12-12 17:52:22 +0400

    Add TASK 2 & TASK 3

--- LECTURE 9/hw9.js
+++ LECTURE 9/hw9.js
@@ -1,10 +1,10 @@
 /// TASK 1
 
-const modal = document.getElementById("modalDIV");
-const btn = document.getElementById("modalBTN");
+const modal = document.getElementById("task1modal");
+const modalBTN = document.getElementById("modalBTN");
 const span = document.getElementsByClassName("close")[0];
 
-btn.onclick = function() {
+modalBTN.onclick = function() {
   modal.style.display = "block";
 }
 
@@ -14,9 +14,58 @@ span.onclick = function() {
 
 /// TASK 2
 
+const colorBTN = document.getElementById("colorBTN");
+colorBTN.onclick = function() {
+    let color = document.getElementById("color").value;
 
-const task2BTN = document.getElementById("task2BTN");
+    if(color == 'red' || color == 'RED'){
+        document.body.style.backgroundColor = 'red';
+    }else if(color == 'blue' || color == 'BLUE'){
+        document.body.style.backgroundColor = 'blue';
+    }else if(color == 'green' || color == 'GREEN'){
+        document.body.style.backgroundColor = 'green';
+    }else if(color == 'black' || color == 'BLACK'){
+        document.body.style.backgroundColor = 'black';
+    }else if(color == 'white' || color == 'WHITE'){
+        document.body.style.backgroundColor = 'white';
+    }else{
+        task2modal.style.display = "block";
+        task2alert();
+    }
+}
 
-task2BTN.onclick = function() {
- document.body.style.backgroundColor = 'red';
+// Wrong color alert
+const task2alert = function (){
+    const task2modal = document.getElementById("task2modal");
+    const task2span = document.getElementsById("task2span");
+}
+
+// Close button for alert
+task2span.onclick = function() {
+    task2modal.style.display = "none";
+}
+
+// Reverse button for background
+const colorReverseBTN = document.getElementById("colorReverseBTN");
+colorReverseBTN.onclick = function() {
+    document.body.style.backgroundColor = 'white';
+}
+
+
+/// TASK 3
+
+const averageBTN = document.querySelector("#averageBTN");
+const inputAverage = document.querySelector("#inputAverage");
+
+averageBTN.addEventListener("click", () => {
+  let inputAverage = document.querySelector("#inputAverage").value;
+  let inputArr = inputAverage.split(";").map((v) => Number(v));
+  let sum = inputArr.reduce((a, b) => a + b, 0);
+  alert.console.log(sum / inputArr.length);
+});
+
+
+// Reset calculator
+resetBTN.onclick = function() {
+    location.reload()  
 }
